49b2dd8fe8892df737659a08b916fa084f99a17f
MichiVIP <michi.developer@gmail.com>
2017-10-27 16:33:17 +0200

    Implement basic MOTD function

---
+++ server/connectionmanagement.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"net"
+	"fmt"
+)
+
+var States map[*net.TCPConn]ConnectionState = make(map[*net.TCPConn]ConnectionState)
+
+type ConnectionState uint8
+
+const HandshakingState ConnectionState = ConnectionState(0)
+const StatusState ConnectionState = ConnectionState(1)
+const LoginState ConnectionState = ConnectionState(2)
+
+type Connection struct {
+	CurrentState ConnectionState
+}
+
+type ErrNoStateFound struct {
+	RawState int
+}
+
+func (errNoStateFound ErrNoStateFound) Error() string {
+	return fmt.Sprintf("could not find any state for value: %v", errNoStateFound.RawState)
+}
+
+func GetConnectionStateFromInt(rawState int) (ConnectionState, error) {
+	switch rawState {
+	case 0:
+		return HandshakingState, nil
+	case 1:
+		return StatusState, nil
+	case 2:
+		return LoginState, nil
+	default:
+		return ConnectionState(0), ErrNoStateFound{RawState: rawState}
+	}
+}
--- server/serversetup.go
+++ server/serversetup.go
@@ -3,13 +3,18 @@ package server
 import (
 	"net"
 	"os"
-	"fmt"
+	"github.com/michivip/mcstatusserver/datatypes"
+	"log"
+	"io"
+	"bytes"
+	"encoding/json"
+	"github.com/satori/go.uuid"
 )
 
 const address = "localhost:25565"
 
 func StartServer() *net.TCPListener {
-	fmt.Printf("Starting server on %v\n", address)
+	log.Printf("Starting server on %v\n", address)
 	tcpAddress, err := net.ResolveTCPAddr("tcp4", address)
 	if err != nil {
 		panic(err)
@@ -34,18 +39,115 @@ func WaitForConnections(listener *net.TCPListener) {
 }
 
 func handleConnection(conn *net.TCPConn) {
-	buffer := make([]byte, 1024)
+	// initial state is Handshaking (http://wiki.vg/Protocol#Definitions)
+	States[conn] = HandshakingState
+	// infinite loop of packet reading
 	for {
-		conn.SetReadBuffer(1024)
-		reqLen, err := conn.Read(buffer)
+		if packet, err, _ := datatypes.ReadPacket(conn); err != nil {
+			if err == io.EOF {
+				log.Printf("Closed connection to %v.", conn.RemoteAddr())
+				break
+			} else {
+				panic(err)
+			}
+		} else {
+			log.Printf("Received packet with id %v from %v.", packet.Id, conn.RemoteAddr())
+			switch packet.Id {
+			case 0:
+				handleHandshakePacket(conn, packet)
+			default:
+				log.Printf("Received packet with unknown ID: %v\n", packet.Id)
+			}
+		}
+	}
+}
+
+type StatusResponse struct {
+	Version struct {
+		Name     string `json:"name"`
+		Protocol int    `json:"protocol"`
+	} `json:"version"`
+	Players struct {
+		Max    int `json:"max"`
+		Online int `json:"online"`
+		Sample []struct {
+			Name string `json:"name"`
+			Id   string `json:"id"`
+		} `json:"sample,omitempty"`
+	} `json:"players"`
+	Description struct {
+		Text string `json:"text"`
+	} `json:"description"`
+	Favicon string `json:"favicon,omitempty"`
+}
+
+func handleHandshakePacket(conn *net.TCPConn, packet datatypes.Packet) {
+	currentState := States[conn]
+	switch currentState {
+	case HandshakingState:
+		version, err, _ := datatypes.ReadVarInt(packet.Content)
 		if err != nil {
-			fmt.Printf("An error occurred while handling incoming data: %v", err)
-			break
+			log.Printf("Received invalid version data from %v: %v", conn.RemoteAddr(), err)
+			return
 		}
-		if reqLen == 0 {
-			break
+		address, err, _ := datatypes.ReadString(packet.Content)
+		if err != nil {
+			log.Printf("Received invalid address data from %v: %v", conn.RemoteAddr(), err)
+			return
 		}
-		fmt.Printf("ReqLen: %v\n", reqLen)
-		fmt.Printf("Message: %v\n", string(buffer))
+		port, err := datatypes.ReadUnsignedShort(packet.Content)
+		if err != nil {
+			log.Printf("Received invalid port data from %v: %v", conn.RemoteAddr(), err)
+			return
+		}
+		nextRawState, err, _ := datatypes.ReadVarInt(packet.Content)
+		var nextState ConnectionState
+		if err != nil {
+			log.Printf("Received invalid next state data from %v: %v", conn.RemoteAddr(), err)
+			return
+		} else if nextState, err = GetConnectionStateFromInt(nextRawState); err != nil {
+			log.Printf("The sent next state from %v is invalid: %v", conn.RemoteAddr(), err)
+			return
+		} else {
+			States[conn] = nextState
+		}
+		log.Printf("Received handshake packet. [version=%v, address=%v, port=%v, nextRawState=%v]\n", version, address, port, nextRawState)
+		return
+	case StatusState:
+		// no additional data is sent which can be read
+		data, err := json.Marshal(&StatusResponse{
+			Version: struct {
+				Name     string `json:"name"`
+				Protocol int    `json:"protocol"`
+			}{Name: "mcstatusserver", Protocol: -1},
+			Players: struct {
+				Max    int `json:"max"`
+				Online int `json:"online"`
+				Sample []struct {
+					Name string `json:"name"`
+					Id   string `json:"id"`
+				} `json:"sample,omitempty"`
+			}{Max: 1337, Online: 0, Sample: []struct {
+				Name string `json:"name"`
+				Id   string `json:"id"`
+			}{{Name: "Hi there, this is", Id: uuid.NewV4().String()}, {Name: "my public server", Id: uuid.NewV4().String()}}},
+			Description: struct{ Text string `json:"text"` }{Text: "§cThis server runs with §aGo§c.\n§7https://github.com/michivip/mcstatusserver"},
+		})
+		if err != nil {
+			panic(err)
+		} else {
+			buffer := bytes.NewBuffer([]byte{})
+			if err, _ := datatypes.WriteString(buffer, string(data)); err != nil {
+				panic(err)
+			}
+			err, _ := datatypes.WritePacket(conn, datatypes.Packet{Content: buffer, Id: 0})
+			if err != nil {
+				panic(err)
+			}
+		}
+		return
+	default:
+		log.Printf("Cannot handle received Handshake packet with current state: %v", currentState)
+		return
 	}
 }
