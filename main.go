86602d860ccaba1a86ba6bf277fcdcce29beb29a
MichiVIP <michivip@users.noreply.github.com>
2017-10-30 02:35:56 +0100

    Implement configuration system

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/michivip/mcstatusserver/server"
+	"flag"
+	"github.com/michivip/mcstatusserver/configuration"
+	"log"
+	"os"
+	"io/ioutil"
+	"encoding/base64"
 )
 
 const asciiArt = "                           _             _                                                           \n" +
@@ -13,8 +19,42 @@ const asciiArt = "                           _             _                    
 	"                                                                                                     "
 
 func main() {
+	configurationFile := flag.String("config", "config.json", "The path to your custom configuration file.")
+	flag.Parse()
+
 	fmt.Println(asciiArt)
-	listener := server.StartServer()
+	config, err := configuration.LoadConfiguration(*configurationFile)
+	if err != nil {
+		log.Printf("There was an error while loading the configuration: %v\n", err)
+		os.Exit(1)
+	}
+	encodedFavicon, err := loadFavicon(config)
+	if err != nil {
+		log.Printf("There was an error while loading the favicon: %v\n", err)
+		os.Exit(1)
+	}
+	config.Motd.FaviconPath = encodedFavicon
+	listener := server.StartServer(config)
 	defer listener.Close()
-	server.WaitForConnections(listener)
+	server.WaitForConnections(listener, config)
+}
+
+func loadFavicon(config *configuration.ServerConfiguration) (string, error) {
+	faviconPath := config.Motd.FaviconPath
+	if faviconPath == "" {
+		return faviconPath, nil
+	}
+	faviconFile, err := os.Open(faviconPath)
+	if err != nil {
+		return faviconPath, err
+	}
+	faviconBytes, err := ioutil.ReadAll(faviconFile)
+	if err != nil {
+		return faviconPath, err
+	}
+	base64Favicon := base64.RawStdEncoding.EncodeToString(faviconBytes)
+	if err := faviconFile.Close(); err != nil {
+		return faviconPath, err
+	}
+	return "data:image/png;base64," + base64Favicon, nil
 }
