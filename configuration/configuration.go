bbb077cf10566fcdfd8e7a924ab06cf5cd25e600
MichiVIP <michivip@users.noreply.github.com>
2017-11-01 02:59:49 +0100

    Release version 1.0.0 (#38)
    * Implement basic server with debug functions (#1)
    
    * Implement feature #3 (#4)
    
    * Add methods for compressed Var-Types (VarInt, VarLong)
    
    * Add methods for String-Type
    
    * Add methods for unsigned short
    
    * Add methods for long
    
    * Simplify read/write methods for unsigned short and long
    
    * Rename method names and fix some typo
    
    * Fix bug #5
    
    * Implement feature #9
    
    * Implement feature of returning amount of written/read bytes
    
    * Fix error of shadowing during return
    
    * Implement packet methods feature #7
    
    * Add PacketId type
    
    * Add Packet write method
    
    * Change PacketId type to general Packet struct type
    
    * Implement read method and change Content field type in Packet struct to a pointer
    
    * Fix error of shadowing during return
    
    * Implement enhancement #8
    
    * Fix bug #15 where unexpected EOF was returned too early
    
    * Fix bug #17 and move some write values to single variables
    
    * Implement feature #19
    
    * Add handler to deny login attempts
    
    * Implement configuration system
    
    * Fix panic crashes and improve logging system (feature #26)
    
    * Adjust README.md and add mcstatusserver files to .gitignore (enhancement #28)
    
    * Fix undetailed panicking (bug #29)
    
    * Implement idle timeout feature (feature #30)
    
    * Adjust json field names (enhancement #31)

---
+++ configuration/configuration.go
@@ -0,0 +1,93 @@
+package configuration
+
+import (
+	"os"
+	"encoding/json"
+	"log"
+)
+
+func LoadConfiguration(fileName string) (*ServerConfiguration) {
+	config := &ServerConfiguration{}
+	file, err := os.Open(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("Creating new configuration file: \"%v\"\n", fileName)
+			file, err = os.Create(fileName)
+			if err != nil {
+				panic(err)
+			}
+			jsonEncoder := json.NewEncoder(file)
+			jsonEncoder.SetIndent("", "  ")
+			if err = jsonEncoder.Encode(getDefaultConfiguration()); err != nil {
+				panic(err)
+			}
+			if err = file.Close(); err != nil {
+				panic(err)
+			}
+			file, err = os.Open(fileName)
+			if err != nil {
+				panic(err)
+			}
+			log.Println("New config file created! Please adjust your values and restart the application.")
+			os.Exit(0)
+		} else {
+			panic(err)
+		}
+	}
+	err = json.NewDecoder(file).Decode(config)
+	if err != nil {
+		panic(err)
+	}
+	if err = file.Close(); err != nil {
+		panic(err)
+	}
+	return config
+}
+
+func getDefaultConfiguration() *ServerConfiguration {
+	return &ServerConfiguration{
+		Address:           "localhost:25565",
+		ConnectionTimeout: 10000,
+		LogFile:           "access.log",
+		LoginAttempt: LoginAttemptValues{
+			DisconnectText: ChatValue{
+				Text:       "You are not ",
+				Bold:       "true",
+				Color:      "red",
+				Insertion:  "true",
+				Underlined: "true",
+				Obfuscated: "true",
+				Italic:     "true",
+				Extra: []ChatComponentValue{
+					{
+						Text:  "allowed to access ",
+						Color: "green",
+					},
+					{
+						Text:   "this server.",
+						Italic: "true",
+					},
+				},
+			},
+		},
+		Motd: MessageOfTheDayValues{
+			Version: struct {
+				Name     string `json:"name"`
+				Protocol int    `json:"protocol"`
+			}{Name: "mcstatusserver 420", Protocol: -1},
+			Players: struct {
+				Max    int `json:"max"`
+				Online int `json:"online"`
+				Sample []struct {
+					Name string `json:"name"`
+					Id   string `json:"id"`
+				} `json:"sample,omitempty"`
+			}{Max: 1337, Online: 42, Sample: []struct {
+				Name string `json:"name"`
+				Id   string `json:"id"`
+			}{{Name: "Hi there, this is", Id: "7cd21442-4bd9-4b02-8539-1a2c4771ed3c"}, {Name: "my public server", Id: "c87a3f54-3802-4227-b72a-17ee3f10cbd9"}}},
+			Description: struct{ Text string `json:"text"` }{Text: "§cThis server runs with §aGo§c.\n§7https://github.com/michivip/mcstatusserver"},
+			FaviconPath: "icon.png",
+		},
+	}
+}
---
+++ configuration/values.go
@@ -0,0 +1,59 @@
+package configuration
+
+type ServerConfiguration struct {
+	Address           string                `json:"address"`
+	ConnectionTimeout int                   `json:"connection-timeout"`
+	LogFile           string                `json:"log-file"`
+	Motd              MessageOfTheDayValues `json:"motd"`
+	LoginAttempt      LoginAttemptValues    `json:"login-attempt"`
+}
+
+// clickEvent or hoverEvent is not needed
+type ChatComponentValue struct {
+	Text          string `json:"text"`
+	Bold          string `json:"bold,omitempty"`
+	Italic        string `json:"italic,omitempty"`
+	Underlined    string `json:"underlined,omitempty"`
+	Strikethrough string `json:"strikethrough,omitempty"`
+	Obfuscated    string `json:"obfuscated, omitempty"`
+	Color         string `json:"color, omitempty"`
+	Insertion     string `json:"insertion, omitempty"`
+}
+
+// clickEvent or hoverEvent is not needed
+type ChatValue struct {
+	Text          string               `json:"text"`
+	Bold          string               `json:"bold,omitempty"`
+	Italic        string               `json:"italic,omitempty"`
+	Underlined    string               `json:"underlined,omitempty"`
+	Strikethrough string               `json:"strikethrough,omitempty"`
+	Obfuscated    string               `json:"obfuscated, omitempty"`
+	Color         string               `json:"color, omitempty"`
+	Insertion     string               `json:"insertion, omitempty"`
+	Extra         []ChatComponentValue `json:"extra, omitempty"`
+}
+
+// MOTD
+type MessageOfTheDayValues struct {
+	Version struct {
+		Name     string `json:"name"`
+		Protocol int    `json:"protocol"`
+	} `json:"version"`
+	Players struct {
+		Max    int `json:"max"`
+		Online int `json:"online"`
+		Sample []struct {
+			Name string `json:"name"`
+			Id   string `json:"id"`
+		} `json:"sample,omitempty"`
+	} `json:"players"`
+	Description struct {
+		Text string `json:"text"`
+	} `json:"description"`
+	FaviconPath string `json:"favicon-path,omitempty"`
+}
+
+// if a user tries to login
+type LoginAttemptValues struct {
+	DisconnectText ChatValue
+}
